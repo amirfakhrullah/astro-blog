7335e91ef401615e6a469a92c8054a791137cbe9
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-10-24 03:39:28 +0800

    blogs mdx done

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,17 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Amir's Blogs";
+export const SITE_DESCRIPTION =
+  "Fullstack Software Engineer and TypeScript enthusiast";
+
+export const CONNECT_LISTS = [
+  {
+    label: "GitHub",
+    link: "https://github.com/amirfakhrullah",
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/amir-fakhrullah-358000199/",
+  },
+];
